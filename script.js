15dc5a31167152fa48bad6889cc0c7c6440ba495
Priti2510 <113327853+Priti2510@users.noreply.github.com>
2024-01-12 12:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,48 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+      if (!this.listeners.has(eventName)) {
+      this.listeners.set(eventName, []);
+    }
+
+    const eventListeners = this.listeners.get(eventName);
+    if (!eventListeners.includes(callback)) {
+      eventListeners.push(callback);
+    }
     }
   
     removeEventListener(event, callback) {
-      
+      if (this.listeners.has(eventName)) {
+      const eventListeners = this.listeners.get(eventName);
+      const index = eventListeners.indexOf(callback);
+
+      if (index !== -1) {
+        eventListeners.splice(index, 1);
+      }
+    }
     }
   
     dispatchEvent(event) {
-    
+    if (this.listeners.has(eventName)) {
+      const eventListeners = this.listeners.get(eventName);
+      for (const callback of eventListeners) {
+        callback();
+      }
+    }
     }
     
   }
-  
+  const target = new EventTarget();
+const logHello = () => console.log('hello');
+const logWorld = () => console.log('world');
+
+target.addEventListener('hello', logHello);
+target.addEventListener('world', logWorld);
+
+target.dispatchEvent('hello');
+target.dispatchEvent('world');
+
+target.removeEventListener('hello', logHello);
+target.dispatchEvent('hello');
+
+target.dispatchEvent('world');
